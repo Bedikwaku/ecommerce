5e1190cab5abff3c0c3a344ce202cfb4a6e3d92a
Kwaku Bediako <abeekubediako@gmail.com>
2024-07-15 17:16:00 +0200

    Updated AWS clients to fix deployment issues

--- src/lib/productService.ts
+++ src/lib/productService.ts
@@ -14,10 +14,6 @@ import "server-only";
 
 const client = new DynamoDBClient({
   region: "us-east-1",
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID!!,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!!,
-  },
 });
 
 const createProduct = async (
